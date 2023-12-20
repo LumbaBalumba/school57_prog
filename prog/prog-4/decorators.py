eaa3c1d50b1cc1c1b93f75af74f156b07d9f97bf
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2023-12-20 10:28:08 +0300

    .

--- prog/prog-4/decorators.py
+++ prog/prog-4/decorators.py
@@ -31,10 +31,53 @@ def benchmark(iters=1):
     return actual_decorator
 
 
+def time_count(f):
+    import time
+
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        res = f(*args, **kwargs)
+        end_time = time.time()
+        return res, end_time - start_time
+
+    return wrapper
+
+
+def squared(f):
+    def wrapper(*args, **kwargs):
+        res = f(*args, **kwargs)
+        return res * res
+
+    return wrapper
+
+
+@time_count
 @count
-@benchmark(iters=5)
+# @benchmark(iters=5)
 def hello(name: str) -> str:
     return f"Hello, {name}"
 
 
+@count
+def abc():
+    return "abc"
+
+
+@squared
+def fact(n):
+    return 1 if n == 0 else n * fact(n - 1)
+
+
+@squared
+def fact_iter(n):
+    res = 1
+    for i in range(2, n + 1):
+        res *= i
+    return res
+
+
 print(hello("admin"))
+print(hello(""))
+print(hello(""))
+print(fact(3))
+print(fact_iter(3))
