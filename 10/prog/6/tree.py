66634f8ac9c8819bdbcc3ed3e1b0e5ab94310417
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-11-14 14:35:07 +0300

    .

--- 10/prog/6/tree.py
+++ 10/prog/6/tree.py
@@ -34,7 +34,7 @@ class Node[T: Comparable]:
         predicate: Callable[[T, T], Compare] = lambda first, second: (
             Compare.Less
             if first < second
-            else (Compare.Equal if first == second else Compare.Greater)
+            else (Compare.Greater if first > second else Compare.Equal)
         ),
     ) -> None:
         self.value = value
@@ -64,6 +64,6 @@ class Node[T: Comparable]:
     to_list(self) -> list[T]
     merge(self, other: Node[T]) -> None
 
-    class Set (add, delete,  &,  | , is_subset,  is_empty, __init__(self, collection: Iterable[T]), Iterable)
-    class Dict([], __init__(self, collection: Iterable[T]), Iterable)
+    class Set (add, delete,  &,  | , is_subset,  is_empty, __init__(self, collection: Iterable[T]), __in__)
+    class Dict([], __init__(self, collection: Iterable[T]), __in__)
     """
