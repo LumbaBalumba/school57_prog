4eefc55f3d43446a4040e790c7d55c35c3910a00
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-11-14 13:10:25 +0300

    nodes prac

--- 10/prog/6/tree.py
+++ 10/prog/6/tree.py
@@ -1,14 +1,21 @@
 from __future__ import annotations
+from enum import Enum, auto
 from typing import Callable
+
+
+class Compare(Enum):
+    Less = auto()
+    Equal = auto()
+    Greater = auto()
 
 
 class Node[T]:
     value: T
     left: Node[T] | None
     right: Node[T] | None
-    predicate: Callable[[T, T], int]
+    predicate: Callable[[T, T], Compare]
 
-    def __init__(self, value: T, predicate: Callable[[T, T], int]) -> None:
+    def __init__(self, value: T, predicate: Callable[[T, T], Compare]) -> None:
         self.value = value
         self.predicate = predicate
         self.left = None
@@ -18,19 +25,23 @@ class Node[T]:
         if self.left is not None:
             if not self.left.correct():
                 return False
-            if not self.predicate(self.left.value, self.value) < 0:
+            if not self.predicate(self.left.value, self.value) == Compare.Less:
                 return False
         if self.right is not None:
             if not self.right.correct():
                 return False
-            if not self.predicate(self.right.value, self.value) > 0:
+            if not self.predicate(self.right.value, self.value) == Compare.Greater:
                 return False
         return True
 
     """
     Написать следующие методы:
     add(self, value: T) -> None
-    search(self, value: T) -> Node[T]
+    search(self, value: T) -> Node[T] | None
     delete(self, value: T) -> None
     to_list(self) -> list[T]
+    merge(self, other: Node[T]) -> None
+
+    class Set
+    class Dict
     """
