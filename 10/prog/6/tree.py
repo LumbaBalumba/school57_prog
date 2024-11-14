078bd666a2ae632208f7a9f719643b5ca0322ddf
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-11-14 10:56:44 +0300

    fix predicate

--- 10/prog/6/tree.py
+++ 10/prog/6/tree.py
@@ -6,9 +6,9 @@ class Node[T]:
     value: T
     left: Node[T] | None
     right: Node[T] | None
-    predicate: Callable[[T, T], bool]
+    predicate: Callable[[T, T], int]
 
-    def __init__(self, value: T, predicate: Callable[[T, T], bool]) -> None:
+    def __init__(self, value: T, predicate: Callable[[T, T], int]) -> None:
         self.value = value
         self.predicate = predicate
         self.left = None
@@ -18,11 +18,19 @@ class Node[T]:
         if self.left is not None:
             if not self.left.correct():
                 return False
-            if not self.predicate(self.left.value, self.value):
+            if not self.predicate(self.left.value, self.value) < 0:
                 return False
         if self.right is not None:
             if not self.right.correct():
                 return False
-            if not self.predicate(self.value, self.right.value):
+            if not self.predicate(self.right.value, self.value) > 0:
                 return False
         return True
+
+    """
+    Написать следующие методы:
+    add(self, value: T) -> None
+    search(self, value: T) -> Node[T]
+    delete(self, value: T) -> None
+    to_list(self) -> list[T]
+    """
