1b70091cd24efe227737491f4793be1f93d64af8
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-11-21 13:22:50 +0300

    .

--- 10/prog/6/tree.py
+++ 10/prog/6/tree.py
@@ -96,4 +96,7 @@ class Node[T: Comparable]:
 
     class Set[T] (add, delete,  &,  | , is_subset,  is_empty, __init__(self, collection: Iterable[T]), __in__)
     class Dict[K, V] ([K] -> V, __init__(self, collection: Iterable[tuple[K, V]]), __contains__)
+
+
+    На дополнительную оценку можно написать тесты для своего кода, покрывающие классы Node, Set, Dict
     """
